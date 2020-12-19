f3793d379c01295140c3f851d3dfdd7b8e65cf9c
fmilla1000 <fmilla1000@gmail.com>
2020-12-19 12:12:42 -0500

    hack10

--- Ejercicios/Ej1.js
+++ Ejercicios/Ej1.js
@@ -4,4 +4,4 @@ let dato1 = prompt("Ingrese su nombre: ")
 let dato2 = prompt("Ingrese su apellido: ")
 let dato3 = prompt("Ingrese su edad: ")
 
-console.log(saludo(dato1, dato2, dato3))
+console.log(fn(dato1, dato2, dato3))
